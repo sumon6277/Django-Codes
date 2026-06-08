055ddab053acdd5266c0a95cc5eb1d015f4762ea
Sumon-Ahmed <sumonahmed72953@gmail.com>
2026-06-08 09:43:09 +0600

    Delete templates_app directory

--- templates_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def about(request):
-    return render(request,'templates_app/about.html')
-def contact(request):
-    return render(request,'templates_app/contact.html')
